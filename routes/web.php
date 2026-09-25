40c29b74d161e19618e73794e1b3730d0d5e5763
René Serrano Pérez <34863443+renedroyd@users.noreply.github.com>
2026-09-25 17:01:16 -0400

    feat: add marketplace products route

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ foreach ((array) config('tenancy.central_domains', []) as $centralDomain) {
     Route::domain($centralDomain)->group(function () {
         Route::get('/', [MarketplaceController::class, 'home'])->name('marketplace.home');
         Route::get('/tiendas', [MarketplaceController::class, 'stores'])->name('marketplace.stores');
+        Route::get('/productos', [MarketplaceController::class, 'products'])->name('marketplace.products');
         Route::get('/registrar-tienda', [MarketplaceController::class, 'register'])->name('marketplace.register');
         Route::post('/registrar-tienda', [MarketplaceController::class, 'registerStore'])->name('marketplace.register.store');
         Route::get('/tiendas/{tenant:slug}', [MarketplaceController::class, 'store'])->name('marketplace.store');
