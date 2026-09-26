4b806b17c7d57a40989140dc29aa0624872189d4
René Serrano Pérez <34863443+renedroyd@users.noreply.github.com>
2026-09-26 00:44:06 -0400

    chore: remove unused database facade

--- app/Http/Controllers/MarketplaceController.php
+++ app/Http/Controllers/MarketplaceController.php
@@ -12,7 +12,6 @@ use App\Models\PlatformUser;
 use App\Models\TenantMembership;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
